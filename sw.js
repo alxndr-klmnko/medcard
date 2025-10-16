e156eeef488861b77171d6b7369e340dcfcd7c9a
Alex <shtorka@MacBook-Pro-Alex.local>
2025-10-16 03:43:45 +0300

    Initial commit - MedCard app files

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'medkarta-v3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/favicon.svg',
+  '/logo192.png',
+  '/logo512.png',
+  '/src/main.tsx',
+  '/src/App.tsx',
+  '/src/index.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
